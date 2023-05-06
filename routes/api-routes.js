2d388c2e6197fb7f0309c5ccfb00d30be2bba5f4
Seer <kmkevins97@gmail.com>
2023-05-05 17:04:42 -0700

    note get request working, notes show up in db and are rendered to page

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
+
+router.get('/api/notes', (req, res) => {
+    const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+    res.json(notesData);
+});
+
+router.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    const newNote = {
+        id: uuidv4(),
+        title,
+        text,
+    };
+
+    const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+    notesData.push(newNote);
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notesData));
+    res.json(newNote);
+});
 
 
 module.exports = router;
