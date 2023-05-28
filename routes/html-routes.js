23186fa8296d7fa0c5db2cadd938a95a8cb94e99
Seer <kmkevins97@gmail.com>
2023-05-27 21:35:53 -0700

    temp removed * route, interfering with db

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,12 +4,13 @@ const path = require('path');
 const router = express.Router();
 
 
+
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-router.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+// router.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../public/index.html'));
+// });
 
 module.exports = router;
